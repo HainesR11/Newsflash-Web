34341e4ed47f56f3d6e041dfdf45ec36047d4fb4
Rhys Haines <rhys.haines@asda.com>
2022-04-04 14:33:46 +0100

    updated navArrow

--- src/components/NavArrow.js
+++ src/components/NavArrow.js
@@ -2,13 +2,17 @@ import React from "react"
 
 const NavArrow = ({ NavText }) => {
     return (
-        <div style={ { display: "flex", flexDirection: "row", marginLeft: "3vw", marginTop: "1vh", marginBottom: "1vh", width: "7vw", height: "7vh", backgroundColor: "grey", overflow: "hidden" } }>
-            <div style={ { width: "7vh", height: "7vh", backgroundColor: "white", zIndex: "2", transform: "rotate(45deg)", marginLeft: "-2.5vw" } }></div>
-            <div style={{display: "flex", alignItems: "center", paddingLeft: "1.5vw"}}>
-                { NavText }
+        <div style={{display: "flex", flexDirection: "row"}}>
+            <div style={ { display: "flex", flexDirection: "row", marginLeft: "30px", marginTop: "10px", marginBottom: "10px", width: "140px", height: "60px", backgroundColor: "grey", overflow: "hidden" } } >
+                <div style={ { width: "60px", height: "60px", backgroundColor: "white", zIndex: "2", transform: "rotate(45deg)", marginLeft: "-25px" } }></div>
+                <div style={ { display: "flex", alignItems: "center", paddingLeft: "25px", zIndex: 3, width: "90px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: 'nowrap'} }>
+                    { NavText }
+                </div>
             </div>
+
+            <div style={ { width: "42px", height: "42px", backgroundColor: "grey", transform: "rotate(45deg)", marginLeft: "-21px", zIndex: "2", marginTop: "19px" } }></div>
         </div>
     )
 }
- 
+
 export default NavArrow
