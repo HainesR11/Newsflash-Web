fdd48c1706fcd7e9bf6db991a33d205ca6a82e5a
Rhys Haines <rhys.haines@asda.com>
2022-04-22 14:46:07 +0100

    apis created

--- src/api/ApiActions.js
+++ src/api/ApiActions.js
@@ -4,13 +4,44 @@ import { useActionStore } from "../store"
 
 
 export const GetAllActions = async () => {
-    //const setActions = useActionStore((state) => state.setActions)
     const setActions = useActionStore.getState().setActions
-    try{
+    try {
         const response = await newsflashServices.get("/api/actions", {})
-         return setActions(response.data)
+        return setActions(response.data)
     }
-    catch(err){
+    catch (err) {
+        console.log(err)
+    }
+}
+
+export const getAction = async (id) => {
+    const setAction = useActionStore.getState().setAction
+    try{
+        const responce = await newsflashServices.get("/api/actions/" + id, {})
+        console.log(responce.data)
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+export const getBuckets = async () => {
+    const setBuckets = useActionStore.getState().setBuckets
+    try {
+        const responce = await newsflashServices.get("/api/buckets", {})
+        return setBuckets(responce.data)
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+export const completeTask = async (id, date, user) => {
+    try {
+        const responce = await newsflashServices.patch("/api/actions/" + id, {  body: {completedOn: date, comepletedBy: user} })
+        return console.log(responce)
+    }
+    catch (err) {
         console.log(err)
     }
 }
