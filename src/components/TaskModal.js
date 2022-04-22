4a14eae559f355b216390533c774f01f3c471a36
Rhys Haines <rhys.haines@asda.com>
2022-04-22 14:46:36 +0100

    modals added

--- src/components/TaskModal.js
+++ src/components/TaskModal.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import { Modal, ModalFooter, ModalHeader } from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.css';
-import { useActionStore, useModalStore } from "../store";
-import styledComponents from "styled-components";
-import DatePicker from "react-date-picker";
+import { useModalStore } from "../store";
 import Select from "react-select";
-import TaskModal from "./TaskModal";
+import styledComponents from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
@@ -26,7 +24,7 @@ const InputBox = styledComponents.input`
     }
 `
 const SummaryInput = styledComponents.textarea`
-    height: 70%;
+    height: 100%;
     border: 0px;
     width: 100%;
     font-size: 15px;
@@ -63,8 +61,10 @@ const optionStyle = {
     })
 }
 
-const AddActivityModal = (show) => {
+const TaskModal = () => {
 
+    const shwoTaskModal = useModalStore((state) => state.showTaskModal)
+    const toggle = useModalStore((state) => state.toggleTaskModaal)
 
     const [activityName, setActivityName] = useState('')
     const [activitySummary, setActivitySummary] = useState("")
@@ -76,59 +76,41 @@ const AddActivityModal = (show) => {
     const [priority, setPriority] = useState()
     const [assigned, setAssigned] = useState([])
 
-    const toggle = useModalStore((state) => state.toggleAddModal)
-    const buckets = useActionStore((state) => state.buckets)
-    const toggleTaskModal = useModalStore((state) => state.toggleTaskModaal)
-
-    const bucket = buckets.map((bucket) => { return { value: bucket.name, label: bucket.name } })
-    console.log(bucket)
+    const priorityList = [
+        { value: "3 - High", label: "3 - High" },
+        { value: "2 - Medium", label: "2 - Medium" },
+        { value: "1 - Low", label: "1 -Low" }
+    ]
 
     return (
-        <TaskModal />,
-        <Modal isOpen={ show } size={ "xl" } style={ { width: "100vw", height: "70vh" } }>
-            <ModalHeader>
-                <div style={ { width: "50vw", display: "flex", flexDirection: "row", justifyContent: "space-between" } }>
-                    <FontAwesomeIcon icon={ faXmark } onClick={ toggle } />
-                    <div style={ { display: "flex", width: "50%" } }>
-                        Create New Action
-                    </div>
+        <Modal size="xl" isOpen={ shwoTaskModal } >
+            <ModalHeader style={ { width: "30vw", display: "flex", flexDirection: "row", justifyContent: "space-between" } }>
+                <div style={ { display: "flex" } }>
+                    Create New Action
                 </div>
+                <FontAwesomeIcon icon={ faXmark } onClick={ toggle } />
             </ModalHeader>
             <div style={ { display: "flex", flexDirection: "column" } }>
                 <div style={ { height: "35vh", width: "100%", display: "flex", flexDirection: "column" } }>
                     <div style={ { width: "100%", textAlign: "center", paddingTop: "10px", fontWeight: "bold" } }>
                         Activty Details
                     </div>
-                    <div style={ { width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-around", height: "43%" } }>
+                    <div style={ { width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-around", height: "50%" } }>
                         <div style={ { width: "47%", height: "25vh", display: "flex", flexDirection: "column", justifyContent: "space-around" } }>
                             <InputBox value={ activityName } placeholder="Activity Name" onChange={ (e) => setActivityName(e.target.value) } />
                             <div style={ { width: "100%" } }>
-                                <Select styles={ optionStyle } options={ bucket } onChange={ setSelectedBucket } isMulti />
-                            </div>
-                            <div style={ { width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-around" } }>
-                                <DatePicker value={ startDate } onChange={ (e) => setStartDate(e) } />
-                                <DatePicker value={ dueDate } place onChange={ (e) => setDueDate(e) } />
+                                <Select styles={ optionStyle } options={ priorityList } onChange={ setPriority } />
                             </div>
                         </div>
-                        <div style={ { width: "47%", height: "100%", marginTop: "2vh" } }>
+                        <div style={ { width: "47%", height: "100%", marginTop: "2vh", display: "flex", justifyContent: "center" } }>
                             <SummaryInput value={ activitySummary } placeholder={ "Activity Summary" } onChange={ (e) => setActivitySummary(e.target.value) } />
-                        </div>
-                    </div>
-                </div>
-                <div style={ { height: "30vh" } }>
-                    <div style={ { display: "flex", flexDirection: "row", width: "98.5%", border: "1px dashed black", paddingTop: "10px", paddingBottom: "10px", marginTop: "20px", borderRadius: "5px", marginLeft: "0.5vw", justifyContent: "space-between" } } onClick={ (toggle, toggleTaskModal) }>
-                        <div style={ { marginLeft: "5vw", fontWeight: "bold" } }>
-                            + Add new Task
-                        </div>
-                        <div style={ { marginRight: "9vw", fontWeight: "bold" } }>
-                            Press Alt + T
                         </div>
                     </div>
                 </div>
             </div>
             <ModalFooter>
                 <div style={ { display: "flex", flexDirection: "row", justifyContent: "space-around", width: "100%" } }>
-                    <button style={ styles.button } onClick={ () => onclick }>Close</button>
+                    <button style={ styles.button } onClick={ toggle }>Close</button>
                     <button style={ styles.button } onClick={ toggle }>Add</button>
                 </div>
             </ModalFooter>
@@ -136,4 +118,4 @@ const AddActivityModal = (show) => {
     )
 }
 
-export default AddActivityModal
+export default TaskModal
