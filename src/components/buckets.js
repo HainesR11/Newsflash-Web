f1b4a4377a20653acb4418a28b5a350f0e9efa91
Rhys Haines <rhys.haines@asda.com>
2022-04-22 14:46:16 +0100

    buckets shown

--- src/components/buckets.js
+++ src/components/buckets.js
@@ -1,32 +1,25 @@
 import React from "react";
 import { useActionStore } from "../store";
-import Switch from "../components/toggle";
 
-const Buckets = ({ buckets }) => {
+const Buckets = () => {
 
     const selectedBucket = useActionStore((state) => state.selectedBucket)
     const setSelectedBucket = useActionStore((state) => state.setSelectedBucket)
+    const buckets = useActionStore((state) => state.buckets)
+
+    console.log(selectedBucket)
 
     return (
         <div style={ { height: "100vh", width: "12vw", display: "flex", flexDirection: "column" } }>
             <div style={{height: "95vh"}}>
                 { buckets.map((buckets) => {
                     return (
-                        <div style={ { color: "white", fontSize: "1vw", width: "9vw", display: "flex", alignItems: "center", marginLeft: "1vw", marginTop: "4vh", paddingTop: "0.5vh", paddingBottom: "0.5vh", paddingLeft: "1vw", borderRadius: selectedBucket === buckets && "5px", border: selectedBucket === buckets && "1px solid white", fontWeight: selectedBucket === buckets && "bold" } } onClick={ () => setSelectedBucket(buckets) }>
-                            { buckets }
+                        <div style={ { color: "white", fontSize: "1vw", width: "9vw", display: "flex", alignItems: "center", marginLeft: "1vw", marginTop: "4vh", paddingTop: "0.5vh", paddingBottom: "0.5vh", paddingLeft: "1vw", borderRadius: selectedBucket === buckets.name && "5px", border: selectedBucket === buckets.name && "1px solid white", fontWeight: selectedBucket === buckets && "bold" } } onClick={ () => setSelectedBucket(buckets.name) }>
+                            { buckets.name }
                         </div>
                     )
                 }) }
             </div>
-            <div style={ { display: "flex", justifyContent: "center", width: "12vw", flexDirection: "row", color: "white", fontSize: "10px", alignItems: "center" } }>
-                <div>
-                    Show Completed
-                </div>
-                <div style={{display: "flex", height: "5vh", alignItems: "center" }}>
-                    <Switch />
-                </div>
-            </div>
-            <div />
         </div>
     )
 }
