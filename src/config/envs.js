9154f843039c04b58c15b327fc1421c19bb4496e
Rhys Haines <rhys.haines@asda.com>
2022-04-22 14:47:24 +0100

    package.json file changed

--- src/config/envs.js
+++ src/config/envs.js
@@ -1,1 +1,17 @@
 const localEnv = "http://localhost:4000"
+const devEnv = "http://localhost:4000"
+const prodEnv = "http://localhost:4000"
+
+export const host = () => {
+    const env = process.env.REACT_APP_NODE_ENV || "dev"
+    switch (env) {
+        case ("prod"):
+            return prodEnv
+        case ("dev"):
+            return devEnv
+        case ("local"):
+            return localEnv
+        default:
+            return localEnv
+    }
+}
