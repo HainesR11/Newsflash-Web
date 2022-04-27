f4f21912d0eb489809e8a8b4ae05c16b17563135
Rhys Haines <rhys.haines@asda.com>
2022-04-27 09:54:49 +0100

    index key added

--- src/components/buckets.js
+++ src/components/buckets.js
@@ -12,9 +12,9 @@ const Buckets = () => {
     return (
         <div style={ { height: "100vh", width: "12vw", display: "flex", flexDirection: "column" } }>
             <div style={{height: "95vh"}}>
-                { buckets.map((buckets) => {
+                { buckets.map((buckets, index) => {
                     return (
-                        <div style={ { color: "white", fontSize: "1vw", width: "9vw", display: "flex", alignItems: "center", marginLeft: "1vw", marginTop: "4vh", paddingTop: "0.5vh", paddingBottom: "0.5vh", paddingLeft: "1vw", borderRadius: selectedBucket === buckets.name && "5px", border: selectedBucket === buckets.name && "1px solid white", fontWeight: selectedBucket === buckets && "bold" } } onClick={ () => setSelectedBucket(buckets.name) }>
+                        <div style={ { color: "white", fontSize: "1vw", width: "9vw", display: "flex", alignItems: "center", marginLeft: "1vw", marginTop: "4vh", paddingTop: "0.5vh", paddingBottom: "0.5vh", paddingLeft: "1vw", borderRadius: selectedBucket === buckets.name && "5px", border: selectedBucket === buckets.name && "1px solid white", fontWeight: selectedBucket === buckets && "bold" } } key={index} onClick={ () => setSelectedBucket(buckets.name) }>
                             { buckets.name }
                         </div>
                     )
